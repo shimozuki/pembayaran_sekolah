924094a86680ca313c9faa28e4877e03816500f9
shimozuki <robbishimozuki@gmail.com>
2023-06-11 14:21:29 +0800

    pembayaran sekolah

---
+++ resources/views/layouts/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright © {{ date('Y') }} <div class="bullet"></div> Created with <i class="fad fa-heart" style="color: red;"></i> by <a href="github.com/shimozuki"
+            target="blank">Shimozuki</a>
+    </div>
+    <div class="footer-right">
+        v0.0.1-Alpha
+    </div>
+</footer>
